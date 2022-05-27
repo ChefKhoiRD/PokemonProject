8fcca1354279f3e55b819766ee0571b616a368d4
TPerschon <timperschon@gmail.com>
2022-05-26 21:34:27 -0600

    Fetching from TCG api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,8 +2,7 @@ var allPokeUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10000';
 var randomPokeUrl = 'https://pokeapi.co/api/v2/pokemon/';
 var cryurl = 'https://veekun.com/dex/media/pokemon/cries/';
 var cry;
-var errorSpeech = new SpeechSynthesisUtterance()
-//errorSpeech.text =
+
 // function to get a random pokemon + cry
 function getPokemon() {
     // get all the pokemon from the API
@@ -12,6 +11,8 @@ function getPokemon() {
         return response.json();
     })
     .then(function(data) {
+        console.log(data)
+        pokemon = data;
         var random;
         var cap;
         // iterate through length of data
@@ -26,12 +27,15 @@ function getPokemon() {
         };
         // generate the random number with the cap
         random = Math.floor(Math.random() * cap);
-        cry = new Audio(`${cryurl}${random}.ogg`)
+        // create a cry audio object using our resource and the random pokemon
+        cry = new Audio(`${cryurl}${random}.ogg`);
+        // lower volume because this stuff really blasts your ears
         cry.volume = .1;
+        // 
         cry.onerror = function() {
             getPokemon();
             return;
-        }
+        };
         cry.play();
         fetch(`${randomPokeUrl}${random}`)
             .then(function(response) {
@@ -41,12 +45,15 @@ function getPokemon() {
                 // set the h1 to the pokemon's name, capitalizing the first letter via charAt and then concatenating the rest of the name via slice, removes hide class keeping element hidden
                 $("#pokename").text(data.species.name.charAt(0).toUpperCase() + data.species.name.slice(1)).removeClass("hide");
                 // set the img to the pokemon's sprite, removes hide class keeping element hidden
-                $("#pokeimg").attr("src", data.sprites.front_default).attr("style", "visibility: visible;").removeClass("hide");
+                $("#pokeimg").attr("src", data.sprites.front_default).removeClass("hide");
             });
     });
 };
 
-$("#generate").click(getPokemon);
+$("#generate").click(function() {
+    getPokemon()
+    return console.log("This is the pokemon" + pokemon)
+});
 
 // function to filter out everything from the URL besides the pokemon number
 function getDataNumber(data) {
@@ -57,3 +64,15 @@ function getDataNumber(data) {
 $("#pokeimg").click(function() {
     cry.play();
 });
+
+fetch('https://api.pokemontcg.io/v2/cards/', {
+    headers: {
+        XApiKey: '6f0066f9-4a35-4bc2-9d6e-cfe8c5948200'
+    }
+})
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
